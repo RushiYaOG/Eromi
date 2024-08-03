982b48fe2ce194113000ae4597e9b7ec83339f7f
RushiYaOG <RSasen7@gmail.com>
2024-08-03 10:02:45 +0530

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "💎 PRIME MUSIC";
-const Text = "GlaceYT";
+const botName = "💎 Eromi";
+const Text = "RushiYaOG";
 const version = "Latest@ v1.0"; 
 const startTime = Date.now();
 
