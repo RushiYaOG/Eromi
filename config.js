57b325ddf990e1741ac5cf018c08151bc7f30b5a
RushiYaOG <RSasen7@gmail.com>
2024-08-03 10:00:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
+║  ## Created by RushiYaOG!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## DISCORD :  https://discord.gg/Y7ztwEJUm2                   ║
+║  ## YouTube : https://www.youtube.com/@RushiYaMC                         ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
@@ -28,8 +28,8 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  activityName: "Music", // This is bot status Write Anything here 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
