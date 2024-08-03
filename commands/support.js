3589b106dcd14fa2295ebd19fe326a99ba9e8e22
RushiYaOG <RSasen7@gmail.com>
2024-08-03 10:50:07 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -37,11 +37,11 @@ module.exports = {
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              iconURL: 'https://cdn.discordapp.com/attachments/1252583672373186606/1269161934280720509/Support.gif?ex=66af0ea9&is=66adbd29&hm=f5dc451514e1ae9f415cced61b909bb699984992ed7bde2e3dabec12b902fb21&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setImage('https://cdn.discordapp.com/attachments/1252583672373186606/1269161934280720509/Support.gif?ex=66af0ea9&is=66adbd29&hm=f5dc451514e1ae9f415cced61b909bb699984992ed7bde2e3dabec12b902fb21&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
